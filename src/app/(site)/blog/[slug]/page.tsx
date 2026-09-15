3ac37ee6dbc0d30f54a3b968f0344afdeaac2403
webbysrilanka-cpu <webbysrilanka@gmail.com>
2026-09-15 23:40:09 +0530

    Add files via upload

--- src/app/(site)/blog/[slug]/page.tsx
+++ src/app/(site)/blog/[slug]/page.tsx
@@ -67,7 +67,7 @@ export default async function PostPage({
     <>
       <script
         type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd).replace(/</g, "\u003c").replace(/>/g, "\u003e").replace(/&/g, "\u0026") }}
       />
 
       <article className="relative pt-[76px]">
